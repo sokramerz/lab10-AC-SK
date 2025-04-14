5ff80d47cc7bc4a1843ab9d1175c5e360eea48c1
Stephen Kramer <10010541@sfcollege.edu>
2025-04-14 19:50:28 -0400

    Added our names and link

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+#https://github.com/sokramerz/lab10-AC-SK
+#Adrian de la Cruz
+#Stephen Kramer
+
 import math
 
 def square_root(a):
@@ -37,26 +41,3 @@ def exponent(a, b):
 
 ###############
 
-def sub(a, b):
-    return a - b
-def mul(a, b):
-
-    return a * b
-
-def div(a, b): # raise ZeroDivisionError if a == 0
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return b / a
-
-def log(a, b):# use math library + raise ValueError
-
-    if a <= 0 or b <= 0:
-        raise ValueError("Both arguments must be positive for logarithm calculation")
-    if a == 1:
-        raise ValueError("Base cannot be 1 for logarithm calculation")
-
-    return math.log(b) / math.log(a)
-
-
-def exp(a, b):
-    return a**b
