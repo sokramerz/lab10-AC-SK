7d883df8c993234877d9d9048fbb68c0b2c595b9
Adrian de la Cruz <adriandelacruz@Adrians-MacBook-Pro.local>
2025-04-14 18:49:45 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -26,3 +26,29 @@ def logarithm(a, b):
 
 def exponent(a, b):
     return a ** b
+
+###############
+
+def sub(a, b):
+    return a - b
+def mul(a, b):
+
+    return a * b
+
+def div(a, b): # raise ZeroDivisionError if a == 0
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):# use math library + raise ValueError
+
+    if a <= 0 or b <= 0:
+        raise ValueError("Both arguments must be positive for logarithm calculation")
+    if a == 1:
+        raise ValueError("Base cannot be 1 for logarithm calculation")
+
+    return math.log(b) / math.log(a)
+
+
+def exp(a, b):
+    return a**b
